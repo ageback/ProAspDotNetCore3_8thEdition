57228353b088aa25b34c608c87ee2124fd03d7e1
Michael Lu <yeskky@gmail.com>
2020-08-07 13:52:38 +0800

    asp.net core 最简单的一个例子

---
+++ FirstProject/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FirstProject.Models;
+
+namespace FirstProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            string viewModel = hour < 12 ? "早上好" : "下午好";
+            return View("MyView", viewModel);
+        }
+    }
+}
