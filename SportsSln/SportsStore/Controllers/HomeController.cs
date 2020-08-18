8a919f7a9c93ae49536e85a6d353732d9cc6481a
Michael Lu <yeskky@gmail.com>
2020-08-18 13:34:01 +0800

    用linq对数据结果分页

--- SportsSln/SportsStore/Controllers/HomeController.cs
+++ SportsSln/SportsStore/Controllers/HomeController.cs
@@ -10,9 +10,12 @@ namespace SportsStore.Controllers
     public class HomeController : Controller
     {
         private IStoreRepository repository;
-
+        public int PageSize = 4;
         public HomeController(IStoreRepository repo) => repository = repo;
-        public IActionResult Index() => View(repository.Products);
+        public ViewResult Index(int productPage = 1) => View(repository.Products
+            .OrderBy(p => p.ProductID)
+            .Skip((productPage - 1) * PageSize)
+            .Take(PageSize));
         
     }
 }
