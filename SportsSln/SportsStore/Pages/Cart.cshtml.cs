d538f7e9e503731d5cddc7ea5103e440ea277dae
Michael Lu <yeskky@gmail.com>
2020-08-20 14:10:00 +0800

    保持购物车会话状态

--- SportsSln/SportsStore/Pages/Cart.cshtml.cs
+++ SportsSln/SportsStore/Pages/Cart.cshtml.cs
@@ -13,9 +13,10 @@ namespace SportsStore.Pages
     {
         private IStoreRepository repository;
 
-        public CartModel(IStoreRepository repo)
+        public CartModel(IStoreRepository repo, Cart cartService)
         {
             repository = repo;
+            Cart = cartService;
         }
 
         public Cart Cart { get; set; }
@@ -24,7 +25,7 @@ namespace SportsStore.Pages
         public void OnGet(string returnUrl)
         {
             ReturnUrl = returnUrl ?? "/";
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            //Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
         }
 
         public IActionResult OnPost(long productId,string returnUrl)
@@ -32,7 +33,7 @@ namespace SportsStore.Pages
             Product product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
             Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
             Cart.AddItem(product, 1);
-            HttpContext.Session.SetJson("cart", Cart);
+            //HttpContext.Session.SetJson("cart", Cart);
             return RedirectToPage(new { returnUrl = returnUrl });
         }
     }
