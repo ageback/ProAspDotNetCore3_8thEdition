2ab026b9e895c33d148216e779c02128959a5bae
Michael Lu <yeskky@gmail.com>
2020-08-21 13:45:58 +0800

    Dockerfile

--- SportsSln/SportsStore/Startup.cs
+++ SportsSln/SportsStore/Startup.cs
@@ -49,7 +49,14 @@ namespace SportsStore
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-
+            if (env.IsProduction())
+            {
+                app.UseExceptionHandler("/error");
+            } else
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
